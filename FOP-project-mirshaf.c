d3d535cb388a0e7a52a75ae78e0fc6706b05f98e
mirshaf <119650737+mirshaf@users.noreply.github.com>
2023-01-14 11:34:25 +0330

    added getline() instead of getstr()

--- FOP-project-mirshaf.c
+++ FOP-project-mirshaf.c
@@ -1,47 +1,47 @@
 #include <stdio.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <stdlib.h>
+#include <sys/stat.h> //getcwd() mkdir()
+#include <string.h> //strcmp()
+#include <stdlib.h> //malloc()
 //#include <unistd.h>
 
-char cwd[1000], tempstr[1000]; //sizeof(cwd) = 1000 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-int eol; //end of line
+#define maxStrInALine 10 //Assuming each line has a maximum of 10 strings and each string has less than 400 characters
+#define maxCharInAStr 400
+char cwd[maxCharInAStr], lineBuff[maxStrInALine][maxCharInAStr]; //sizeof(cwd) = 400
 
-int getstr(char *out){ //Ignores the beginning spaces. handles "str". puts a '\0' at the end of *out. Assumes sizeof(out) is enough. eliminates 1 whitespace after the word in the input. does not write '\n'.
+char whitespace(){ //ignore the spaces
     char c = ' ';
     while(c == ' ') c = getchar();
-    int i = 0;
-    if(c == '"'){
-        while((c=getchar()) != '"'){ //I'm expecting it to end with a "
-            out[i] = c;
-            i++;
-            if(c == '\n'){
-                out[i] = '\0';
-                return -1; //error
-            }
-        }
-        c = getchar();
-        if((c != ' ') && (c != '\n')){
-            out[i] = '\0';
-            return -1; //error
-        }
-    }
-    else if(c == '\n'){
-
-    }
-    else{
-        while((c != ' ') && (c != '\n')){
-            out[i] = c;
-            i++;
-            c = getchar();
-        }
-    }
-    out[i] = '\0';
-    if(c == '\n') return 1;
-    return 0; //Success
-    //returns 0 if the word ends with a space, 1 if it ends with a '\n' and -1 if an error occurs.
+    return c;
 }
+int getline(char out[maxStrInALine][maxCharInAStr]){ //returns the number of strings in the line
+    char c;
+    int currentStr = 0, currentChar = 0;
+    while( (c = whitespace()) != '\n'){
+        currentChar = 0;
+        if(c == '"'){
+            while((c = getchar()) != '"'){
+                if(c == '\n') return -1;
+                out[currentStr][currentChar] = c;
+                currentChar++;
+            }
+            out[currentStr][currentChar] = '\0';
+        }
+        else{
+            do{
+                if(c == '\n'){
+                    out[currentStr][currentChar] = '\0';
+                    return currentStr+1;
+                }
+                out[currentStr][currentChar] = c;
+                currentChar++;
+            }while((c = getchar()) != ' ');
 
+            out[currentStr][currentChar] = '\0';
+        }
+        currentStr++;
+    }
+    return currentStr;
+}
 void error(char *message){
     if(message[0] == '\0'){
         printf("Error.\n");
@@ -59,28 +59,11 @@ int main(){
 
     //char com[50]; //command aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     while(1){
-        eol = getstr(tempstr);
-        if(!strcmp(tempstr, "createfile")){ //returns 0 if the two strings are the same
-            if(eol == 1){
-                error("'createfile' requires a '--file' flag.");
-            }
-            else{
-                eol = getstr(tempstr);
-                if(!strcmp(tempstr, "--file")){
-                    eol = getstr(tempstr);
-
-                }
-                else if(tempstr[0] == '\0'){
-                    error("'createfile' requires a '--file' flag."); //flag-option-attribute
-                }
-                else{
-                    error("Not recognized. 'createfile' requires a '--file' flag.");
-                    while(eol != 1) eol = getstr(tempstr);
-                }
-            }
+        int nos = getline(lineBuff); //Number of strings(words)
+        for(int i = 0; i < nos; i++){
+            printf("'%s' ", lineBuff[i]);
         }
-
-
+        printf("\n");
     }
 }
 
